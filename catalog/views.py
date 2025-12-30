79c6e967c91fa67312a524efd83f5480747c2a50
Nikolai <sir.nikolai00@mail.ru>
2025-12-30 21:52:31 +0300

    Сделяль

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home_open(request):
+    return render(request,"catalog/home.html")
+
+def contacts_open(request,):
+    context={
+        "title":'Контакты',
+        "message":'ВЫ во вкладке контакты!'
+    }
+    return render(request,"catalog/contacts.html",context)
+
+def catalog_open(request):
+    return render(request,"catalog/product-catalog.html")
